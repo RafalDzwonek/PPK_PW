89edcb3250c95dfebd4ecfd57a82c0a481d2b433
RafalDzwonek <34712373+RafalDzwonek@users.noreply.github.com>
2018-09-20 10:57:30 +0200

    Dodaj pliki projektów.

---
+++ WindowsFormsApp1/Form1.Designer.cs
@@ -0,0 +1,101 @@
+﻿namespace WindowsFormsApp1
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Wymagana zmienna projektanta.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Wyczyść wszystkie używane zasoby.
+        /// </summary>
+        /// <param name="disposing">prawda, jeżeli zarządzane zasoby powinny zostać zlikwidowane; Fałsz w przeciwnym wypadku.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Kod generowany przez Projektanta formularzy systemu Windows
+
+        /// <summary>
+        /// Metoda wymagana do obsługi projektanta — nie należy modyfikować
+        /// jej zawartości w edytorze kodu.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.gameField1 = new WindowsFormsApp1.GameField();
+            this.SuspendLayout();
+            // 
+            // timer1
+            // 
+            this.timer1.Enabled = true;
+            this.timer1.Interval = 1;
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(12, 370);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(218, 33);
+            this.button1.TabIndex = 5;
+            this.button1.Text = "Restart";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click_1);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(265, 370);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(218, 33);
+            this.button2.TabIndex = 6;
+            this.button2.Text = "Quit";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click_1);
+            // 
+            // gameField1
+            // 
+            this.gameField1.BackColor = System.Drawing.SystemColors.Window;
+            this.gameField1.Location = new System.Drawing.Point(12, 12);
+            this.gameField1.Name = "gameField1";
+            this.gameField1.Size = new System.Drawing.Size(471, 341);
+            this.gameField1.TabIndex = 4;
+            this.gameField1.Paint += new System.Windows.Forms.PaintEventHandler(this.gameField1_Paint);
+            this.gameField1.MouseClick += new System.Windows.Forms.MouseEventHandler(this.gameField1_MouseClick);
+            this.gameField1.MouseMove += new System.Windows.Forms.MouseEventHandler(this.gameField1_MouseMove);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.HotTrack;
+            this.ClientSize = new System.Drawing.Size(495, 413);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.gameField1);
+            this.Name = "Form1";
+            this.Text = "Moja aplikacja";
+            this.Load += new System.EventHandler(this.Panel_Load);
+            this.KeyDown += new System.Windows.Forms.KeyEventHandler(this.Form1_KeyDown_1);
+            this.KeyUp += new System.Windows.Forms.KeyEventHandler(this.Form1_KeyUp);
+            this.PreviewKeyDown += new System.Windows.Forms.PreviewKeyDownEventHandler(this.Form1_PreviewKeyDown);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+        private GameField gameField1;
+        private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+    }
+}
+
---
+++ WindowsFormsApp1/Form1.cs
@@ -0,0 +1,241 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+
+namespace WindowsFormsApp1
+
+
+{
+    public partial class Form1 : Form
+    {
+        class Bullet
+        {
+            int x, y, w, h;
+
+        }
+
+        int x, y, w, h;
+        int direction, speed;
+        int enemyX, enemyY, enemyDirection;
+        float enemyFall, rate;
+        bool hit = false, dead = false;
+        int amoSize, next;
+        int countdown;
+        string str;
+        Rectangle[] Amo;
+        int[] goup;
+        Rectangle box;
+        Bitmap shipModel, enemyModel;
+        Font hitFont = new Font("Arial", 10);
+
+        public Form1()
+        {
+            InitializeComponent();
+            this.KeyPreview = true;
+            next = 0;
+            direction = 0;
+            speed = 3;
+            x = gameField1.Width / 2;
+            w = h = 10;
+            y = gameField1.Height - h;
+            Init_Amo();
+            try
+            {
+                shipModel = (Bitmap)Image.FromFile(@"C:\Users\rafal\source\repos\WindowsFormsApp1\WindowsFormsApp1\ship.bmp", true);
+                enemyModel = (Bitmap)Image.FromFile(@"C:\Users\rafal\source\repos\WindowsFormsApp1\WindowsFormsApp1\enemy.bmp", true);
+            }
+            catch (System.IO.FileNotFoundException)
+            {
+                MessageBox.Show("Nie ma pliku");
+            }
+
+        }
+
+        public void Collision()
+        {
+            for (int i=0; i < amoSize; i++)
+            {
+                if(Amo[i].Y < enemyY + enemyModel.Height && Amo[i].X > enemyX && Amo[i].X < enemyX + enemyModel.Width)
+                {
+                    hit = true;
+                    enemyDirection = 0;
+                }
+            }
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+
+        private void Form1_PreviewKeyDown(object sender, PreviewKeyDownEventArgs e)
+        {
+            if (e.KeyCode == Keys.Up || e.KeyCode == Keys.Down || e.KeyCode == Keys.Left || e.KeyCode == Keys.Right || e.KeyCode == Keys.Space)
+            {
+                e.IsInputKey = true;
+            }
+        }
+
+        private void Move(Keys key)
+        {
+            switch (key)
+            {
+               case Keys.Right:
+                    direction = speed;
+                    break;
+
+                case Keys.Left:
+                    direction = -speed;
+                    break;
+
+                case Keys.Space:
+                    if (next < amoSize)
+                    {
+                        goup[next] = -1;
+                        next++;
+                    }
+                    else { next = 0; }
+                    break;
+            }
+        }
+
+        private void Form1_KeyDown_1(object sender, KeyEventArgs e)
+        {
+            Move(e.KeyCode);
+        }
+
+        private void Form1_KeyUp(object sender, KeyEventArgs e)
+        {
+            if (e.KeyCode == Keys.Up || e.KeyCode == Keys.Down || e.KeyCode == Keys.Left || e.KeyCode == Keys.Right)
+            {
+                direction = 0;
+            }
+        }
+
+        private void button2_Click_1(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+
+        private void button1_Click_1(object sender, EventArgs e)
+        {
+            Form1 NewForm = new Form1();
+            NewForm.Show();
+            this.Dispose(false);
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            enemyX += enemyDirection;
+            enemyFall += 0.2f;
+            enemyY = (int)enemyFall;
+            if (enemyX >= gameField1.Width - enemyModel.Width)
+            { enemyDirection = -1; }
+            if (enemyX <= 0)
+            { enemyDirection = 1; }
+            gameField1.Refresh();
+        }
+
+        private void Panel_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void gameField1_MouseMove(object sender, MouseEventArgs e)
+        {
+            x = e.X;
+        }
+
+        private void gameField1_MouseClick(object sender, MouseEventArgs e)
+        {
+            if (next < amoSize)
+            {
+                goup[next] = -1;
+                next++;
+            }
+            else { next = 0; }
+        }
+
+        private void gameField1_Paint(object sender, PaintEventArgs e)
+        {
+            Graphics g = e.Graphics;
+            SolidBrush p = new SolidBrush(Color.Black);
+            Color backColor = Color.White;
+            x += direction;
+            shipModel.MakeTransparent(backColor);
+            g.DrawImage(shipModel, x - 7, y - shipModel.Height + 15);
+            enemyModel.MakeTransparent(backColor);
+            Collision();
+            if (!dead)
+            {
+                if (hit == false)
+                {
+                    g.DrawImage(enemyModel, enemyX, enemyY);
+                }
+                else
+                {
+                    g.DrawString("Trafiony", hitFont, p, enemyX, enemyY);
+                    kill();
+                }
+            }
+            setAmoXY();
+            AmoRefresh(g);
+        }
+
+        public async void kill()
+        {
+            await Task.Delay(2000);
+            dead = true;
+
+        }
+
+        public void setBox(int x)
+        {
+            box.X = x - w;
+            box.Width = 2 * w;
+            box.Height = 2 * h;
+        }
+     
+        private void Init_Amo()
+        {
+            amoSize = 10;
+            Amo = new Rectangle[amoSize];
+            goup = new int[amoSize];
+            for (int i = 0; i < amoSize; i++)
+            {
+                Amo[i] = new Rectangle(x-1, y, 2, 3);
+                goup[i] = 0;
+
+            }
+        }
+
+        public void setAmoXY()
+        {
+            for (int i = 0; i < amoSize; i++)
+            {
+                Amo[i].Y += goup[i];
+                if (goup[i] == 0) { Amo[i].X = x -1; }
+                if (Amo[i].Y <= 0) { Amo[i].X = x - (w - 5) / 2; Amo[i].Y = y - h; goup[i] = 0; }
+            }
+        }
+
+        public void AmoRefresh(Graphics g)
+        {
+            SolidBrush pa = new SolidBrush(Color.Red);
+            for (int i = 0; i < amoSize; i++)
+            {
+                g.FillRectangle(pa, Amo[i]);
+                g.DrawRectangle(new Pen(pa), Amo[i]);
+            }
+        }
+
+    }
+}
